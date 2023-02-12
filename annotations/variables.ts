5ad857858576f281d39035f17e7b60a1a6704055
Koushik Dey <koushik.dey@live.com>
2023-02-13 00:01:31 +0200

    annotations

---
+++ annotations/variables.ts
@@ -0,0 +1,42 @@
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+// built in objects
+
+let date: Date = new Date();
+
+// Array
+
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let truths: boolean[] = [true, false, true, false, false];
+
+// classes
+
+class Car {
+
+};
+
+let car: Car = new Car();
+
+// Object literal
+
+let point: { x: number; y: number } = {
+  x: 10,
+  y: 20
+};
+
+// Function
+
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+
+
+
+
