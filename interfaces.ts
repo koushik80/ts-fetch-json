4bd4c65162d2421c4657a2604681b9b56f97aefb
Koushik Dey <koushik.dey@live.com>
2023-02-13 15:23:00 +0200

    interfaces

---
+++ interfaces.ts
@@ -0,0 +1,57 @@
+/*
+interface Vehicle {
+  name: string;
+  year: Date;
+  broken: boolean;
+  summary(): string;
+};
+
+const oldCivic = {
+  name: 'civic',
+  year: new Date(),
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  }
+};
+
+const printVehicle = (vehicle: Vehicle): void => {
+  //console.log(`Name: ${vehicle.name}`);
+  //console.log(`Year: ${vehicle.year}`);
+  //console.log(`Broken? ${vehicle.broken}`);
+  console.log(vehicle.summary());
+};
+
+printVehicle(oldCivic);
+*/
+
+// Reusable code
+
+interface Reportable {
+  summary(): string;
+};
+
+const oldCivic = {
+  name: 'civic',
+  year: new Date(),
+  broken: true,
+  summary(): string {
+    return `Name: ${this.name}`;
+  }
+};
+
+const drink = {
+  color: 'brown',
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar.`;
+  }
+};
+
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary());
+};
+
+printSummary(oldCivic);
+printSummary(drink);
