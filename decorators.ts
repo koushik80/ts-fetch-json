c20c1b97d9ccb99e011b094f3665b3ef1083e1d6
Koushik Dey <koushik.dey@live.com>
2023-03-07 14:54:13 +0200

    decorators arround properties

--- decorators.ts
+++ decorators.ts
@@ -1,30 +1,45 @@
+@classDecorator
 class Boat {
+  @testDecorator
   color: string = 'red';
 
+  @testDecorator
   get formattedColor(): string {
     return `This boats color is ${this.color}`;
   }
 
-  //@testDecorator
-  @logError
-  pilot(): void {
-    throw new Error();
-    console.log('swish')
-  }
-}
-
-function logError(target: any, key: string, desc: PropertyDescriptor): void {
-  const method = desc.value;
-
-  desc.value = function () {
-    try {
-      method();
-    } catch (e) {
-      console.log('Oops, Boat was sunk!')
+  @logError('Oops boat was sunk in ocean')
+  pilot(@parameterDecorator speed: string, @parameterDecorator generateWake: boolean): void {
+    if (speed === 'fast') {
+      console.log('swish')
+    } else {
+      console.log('nothing')
     }
   }
 }
 
-new Boat().pilot();
+function parameterDecorator(target: any, key: string, index: number) {
+  console.log(key, index);
+}
 
-//testDecorator(Boat.prototype, 'pilot');
+function classDecorator(constructor: typeof Boat) {
+   console.log(constructor);
+}
+
+function testDecorator(target: any, key: string) {
+  console.log(key);
+}
+
+function logError(errorMessage: string) {
+  return function (target: any, key: string, desc: PropertyDescriptor): void {
+    const method = desc.value;
+
+    desc.value = function () {
+      try {
+        method();
+      } catch (e) {
+        console.log(errorMessage);
+      }
+    }
+  }
+}
